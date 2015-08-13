d6b3469f8436cfa1f4c4df273e1ee29f73a9fff6
Pomin Wu <pomin5@gmail.com>
2015-08-13 16:24:42 +0800

    fix update rm path

--- script/update.sh
+++ script/update.sh
@@ -3,6 +3,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd ${DIR}
-rm -f src/posts/* src/authors/*
+rm -f ../src/posts/* ../src/authors/*
 babel-node update-posts.es6 $*
 babel-node update-authors.es6 $*
